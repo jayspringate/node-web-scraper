56c49e80dcf0ed3445fdb16d4170823c95260005
jayspringate <jay.springate@gmail.com>
2015-07-31 21:27:15 -0700

    clean up file structure, naming, and formatting, add test folder

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ var fs = require('fs');
 var request = require('request');
 var cheerio = require('cheerio');
 var app = express();
-var scraper = require('./scraper')
+var scraper = require('./scraper');
 
 app.get('/scrape', scraper);
 
-app.listen('3000')
+app.listen('3000');
 
 console.log('server listening on port 3000');
 
