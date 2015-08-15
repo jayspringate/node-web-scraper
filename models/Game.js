0bc46f2e01dd70585addf2a77e2aeaea3f21beb3
jayspringate <jay.springate@gmail.com>
2015-08-14 23:15:22 -0700

    add columns to schema, insert columns in scrape

--- models/Game.js
+++ models/Game.js
@@ -7,7 +7,8 @@ var sql = new Sql('wagermetrics_dev', 'wagermetrics_dev',
 var Game = module.exports = sql.define ('Game', {
   date: Sql.DATEONLY,
   eventId: {type: Sql.STRING,
-            unique: true
+            unique: true,
+            primaryKey: true
           },
   team: Sql.STRING,
   opponent: Sql.STRING,
@@ -19,12 +20,17 @@ var Game = module.exports = sql.define ('Game', {
   totalOpen: Sql.FLOAT,
   totalClose: Sql.FLOAT,
   season: Sql.STRING,
-  gameType: Sql.STRING
+  gameType: Sql.STRING,
+  atsGrade: Sql.STRING,
+  suGrade: Sql.STRING,
+  totalGrade: Sql.STRING,
+  spreadMove: Sql.STRING,
+  totalMove: Sql.STRING
 },
 
 {
   timestamps: false,
-  tableName: 'NbaGamesTest'
+  tableName: 'NbaGamesTestDecember'
 });
 
 Game.sync();
