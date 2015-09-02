4ee75cecf38c018a746aed6e3509fe1d426b9b9e
jayspringate <jay.springate@gmail.com>
2015-09-02 03:30:15 -0700

    add phantomjs scrape

--- server.js
+++ server.js
@@ -4,9 +4,11 @@ var express = require('express');
 var app = express();
 var nbaScraper = require('./sports/nba/nba-scraper');
 var cfbScraper = require('./sports/cfb/cfb-scraper');
+var phantom = require('./phantom-scrape');
 
 app.get('/nbascrape', nbaScraper);
 app.get('/cfbscrape', cfbScraper);
+app.get('/phantom', phantom);
 
 app.listen('3000', function() {
   console.log('server listening on port 3000');
